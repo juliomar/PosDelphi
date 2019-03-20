cbdec2cfe653c759a1b5b845dffff56a5abcc13e
David M de Lima <davidlima.dev@gmail.com>
2019-03-20 00:10:28 -0300

    master - conflito

--- Projeto/PosDelphi.dpr
+++ Projeto/PosDelphi.dpr
@@ -1,4 +1,4 @@
-{*******************************************************}
+﻿{*******************************************************}
 {                                                       }
 {       Projeto Teste P�s-Delphi                        }
 {                                                       }
@@ -21,13 +21,14 @@ uses
   Controller.Interfaces in 'Controller\Controller.Interfaces.pas',
   Model.Cadastro in 'Model\Cadastro\Model.Cadastro.pas',
   Model.Cadastro.Interfaces in 'Model\Cadastro\Model.Cadastro.Interfaces.pas',
-  Controller.Cadastro.Pessoa in 'Controller\Controller.Cadastro.Pessoa.pas',
   Model.Exportador.Interfaces in 'Model\Exportador\Model.Exportador.Interfaces.pas',
   Model.Exportador.InterfaceFormato in 'Model\Exportador\Model.Exportador.InterfaceFormato.pas',
   Model.Exportador.Alunos in 'Model\Exportador\Model.Exportador.Alunos.pas',
   Model.Exportador.FormatoHTML in 'Model\Exportador\Model.Exportador.FormatoHTML.pas',
-  Model.Exportador.FormatoXLS in 'Model\Exportador\Model.Exportador.FormatoXLS.pas';
-  Controller.Cadastro in 'Controller\Controller.Cadastro.pas',
+  Model.Exportador.FormatoXLS in 'Model\Exportador\Model.Exportador.FormatoXLS.pas',
+  Model.Iterator.Interfaces in 'Model\Iterator\Model.Iterator.Interfaces.pas';
+
+Controller.Cadastro in 'Controller\Controller.Cadastro.pas',
   Model.Iterator.Interfaces in 'Model\Iterator\Model.Iterator.Interfaces.pas',
   Model.Lista.Factory in 'Model\Iterator\Model.Lista.Factory.pas',
   Model.Lista.List in 'Model\Iterator\Model.Lista.List.pas',
