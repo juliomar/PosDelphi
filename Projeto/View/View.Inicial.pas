d7d98a175218ad307566135fcbba4045d77b2595
Regiane <regianebaddi@hotmail.com>
2019-04-02 15:14:47 -0300

    Alteração na parte gráfica do form inicila
    Melhorias na parte gráfica do form inicial

--- Projeto/View/View.Inicial.pas
+++ Projeto/View/View.Inicial.pas
@@ -6,7 +6,8 @@ uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
   System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.ComCtrls,
-  Vcl.Imaging.jpeg, Vcl.StdCtrls, View.Principal, View.Tabela.Cursos;
+  Vcl.Imaging.jpeg, Vcl.StdCtrls, View.Principal, View.Tabela.Cursos,
+  Vcl.Imaging.pngimage;
 
 type
   TfrmTelaDeInicio = class(TForm)
@@ -15,6 +16,9 @@ type
     TimerTelaInicial: TTimer;
     BtnTelaPrincipal: TButton;
     BtnCursos: TButton;
+    Image1: TImage;
+    Image2: TImage;
+    Label1: TLabel;
     procedure TimerTelaInicialTimer(Sender: TObject);
     procedure BtnTelaPrincipalClick(Sender: TObject);
     procedure BtnCursosClick(Sender: TObject);
@@ -68,7 +72,7 @@ end;
 
 procedure TfrmTelaDeInicio.TimerTelaInicialTimer(Sender: TObject);
 begin
-  StatusBarTelaInicial.Panels.Items[0].Text := DateToStr(Now);
+  StatusBarTelaInicial.Panels.Items[0].Text := DateTimeToStr(now);;
 end;
 
 end.
